29063013e3ae7c42399c6fde9cf2320d7811fdf3
Miguel <miguelg71921@gmail.com>
2024-07-30 23:01:46 -0700

    retry

--- src/components/Paintings.js
+++ src/components/Paintings.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ImageGrid from './ImageGrid';
 
 const images = [
-    { src: `/sol_website/images/paintings/ring.png`, alt: "Green ring pop", span: "col-span-1 row-span-1", bgColor: "bg-gray-100" },
-    { src: `/sol_website/images/paintings/candy.png`, alt: "Blue wrapped candy", span: "col-span-1 row-span-1", bgColor: "bg-gray-100" },
+    { src: `/images/paintings/ring.png`, alt: "Green ring pop", span: "col-span-1 row-span-1", bgColor: "bg-gray-100" },
+    { src: `/images/paintings/candy.png`, alt: "Blue wrapped candy", span: "col-span-1 row-span-1", bgColor: "bg-gray-100" },
 ];
 
 // console.log(process.env.PUBLIC_URL);

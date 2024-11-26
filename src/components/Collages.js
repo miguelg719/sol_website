fc76feba968dd302a5b3a05f6baa30b29d60460b
Miguel <miguelg71921@gmail.com>
2024-11-25 16:14:59 -0800

    migrate back to gh pages

--- src/components/Collages.js
+++ src/components/Collages.js
@@ -3,10 +3,10 @@ import React from 'react';
 import ImageGrid from './ImageGrid';
 
 const images = [
-  { src: '/images/collages/collage_1.png', alt: "Fruit arrangement", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Tabachines", description: "Digital collage, 2024" },
-  { src: '/images/collages/collage_2.png', alt: "Pomegranate collage", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "La dulce fortuna", description: "Digital collage, 2024"},
-  { src: '/images/collages/collage_3.png', alt: "Pomegranate collage", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "El sol y sus aves", description: "Digital collage, 2024"},
-  { src: '/images/collages/collage_4.png', alt: "Pomegranate collage", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Hotel Bali Hai", description: "Digital collage, 2024"},
+  { src: './images/collages/collage_1.png', alt: "Fruit arrangement", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Tabachines", description: "Digital collage, 2024" },
+  { src: './images/collages/collage_2.png', alt: "Pomegranate collage", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "La dulce fortuna", description: "Digital collage, 2024"},
+  { src: './images/collages/collage_3.png', alt: "Pomegranate collage", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "El sol y sus aves", description: "Digital collage, 2024"},
+  { src: './images/collages/collage_4.png', alt: "Pomegranate collage", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Hotel Bali Hai", description: "Digital collage, 2024"},
 ];
 
 const Collages = () => (
--- src/components/Printmaking.js
+++ src/components/Printmaking.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ImageGrid from './ImageGrid';
 
 const images = [
-  { src: '/images/printmaking/taza.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "María", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/chuches.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Bety", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/legs.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Andrés", description: "Grabado en punta seca, 2023", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/ballerina.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Gúerita", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/cigarro.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Cecilia", description: "Grabado en punta seca, 2023", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/paleta1.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Sergio", description: "Grabado en punta seca, 2023", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/paleta2.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Aurora", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
-  { src: '/images/printmaking/dona.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Miguel", description: "Grabado en punta seca, 2024", dimensions:" 26x35 cm" }
+  { src: './images/printmaking/taza.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "María", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/chuches.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Bety", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/legs.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Andrés", description: "Grabado en punta seca, 2023", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/ballerina.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Gúerita", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/cigarro.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Cecilia", description: "Grabado en punta seca, 2023", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/paleta1.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Sergio", description: "Grabado en punta seca, 2023", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/paleta2.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Aurora", description: "Grabado en punta seca, 2024", dimensions:" 35x26 cm" },
+  { src: './images/printmaking/dona.png', alt: "Legs at the beach", span: "col-span-1 row-span-1", bgColor: "bg-gray-100", title: "Miguel", description: "Grabado en punta seca, 2024", dimensions:" 26x35 cm" }
 ];
 
 const Printmaking = () => (
